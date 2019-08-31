bd09634889000660cb8ab4d8ecb0de0bf948efa2
Sylvanus Kateile <kateile@outlook.com>
2019-08-31 12:11:18 +0300

    add custom config file

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This is represent your college/university.
+ * (32 characters length)
+ */
+define('API_ID', '');
+
+/**
+ * Database name were university/college/students data are stored.
+ * (64 characters length).
+ * This should be kept private.
+ */
+define('API_SECRET', '');
+
+/**
+ * full URL where this app will be hosted.
+ * eg https://amazing-university.org/nyererefy_bridge.
+ */
+define('BASE_URL', 'http://localhost/bridge');
+
+/**
+ * Your host.
+ */
+define('DB_HOST', 'localhost');
+
+/**
+ * Mysql username.
+ */
+define('DB_USERNAME', 'root');
+
+/**
+ * Mysql password.
+ */
+define('DB_PASSWORD', '');
+
+/**
+ * Database name were university/college/students data are stored.
+ * replace [bridge_test_students_db] with your database name
+ */
+define('DB_NAME', 'bridge_test_students_db');
+
+/**
+ * Database driver.
+ * Change it only if you are using different database. Default driver is for Mysql.
+ */
+define('DB_DRIVER', 'mysqli');
+
