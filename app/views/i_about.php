8c7b7539bdd32c82cea34ffd77291171e3404653
Sylvanus Kateile <kateile@outlook.com>
2019-11-23 18:47:53 +0300

    edit about page

--- app/views/i_about.php
+++ app/views/i_about.php
@@ -1,7 +1,11 @@
-<!-- Jumbotron Header -->
 <header class="jumbotron my-4">
     <h1 class="display-5">So what is to Nyererefy Bridge?</h1>
-    <p class="lead">This is Just a simple script running on your University/College's
+    <p class="lead">This is Just a simple script running on <b><?= INSTITUTION_ABBREVIATION_NAME ?></b>
         server intended to help you sending your data to Nyererefy's server when you want to.
+    </p>
+    <h1 class="display-5">What data does Nyererefy need?</h1>
+    <p class="lead">We only need your Registration number, Email and Class which
+        helps us filtering elections categories you are eligible
+        to vote.
     </p>
 </header>
