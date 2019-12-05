e81f01933a496746a7b4cc9c289e2866b4d4af8b
Sylvanus Kateile <kateile@outlook.com>
2019-12-05 05:14:27 +0300

    add some points in about page

--- app/views/i_about.php
+++ app/views/i_about.php
@@ -1,11 +1,16 @@
 <header class="jumbotron my-4">
     <h1 class="display-5">So what is to Nyererefy Bridge?</h1>
-    <p class="lead">This is Just a simple script running on <b><?= INSTITUTION_ABBREVIATION_NAME ?></b>
+    <p class="lead text-left">This is Just a simple script running on <b><?= INSTITUTION_ABBREVIATION_NAME ?></b>
         server intended to help you sending your data to Nyererefy's server when you want to.
     </p>
     <h1 class="display-5">What data does Nyererefy need?</h1>
-    <p class="lead">We only need your Registration number, Email and Class which
-        helps us filtering elections categories you are eligible
-        to vote.
+    <p class="lead text-left">We only need your Registration number (<i>For identity</i>),
+        Email (<i>For login</i>) and Class (<i>For filtering elections categories you are eligible
+            to vote</i>).
+    </p>
+    <h1 class="display-5">Does Nyererefy need my real identity?</h1>
+    <p class="lead text-left">We <b>don't</b> need/transfer your personal info. You can choose whatever names you like
+        during
+        account setup. Only Candidates are required to use their real names.
     </p>
 </header>
